97cead41c27c857ba4e46c154a94f9e2787d2966
Tobias Gesellchen <tobias@gesellix.de>
2013-11-19 21:52:32 +0100

    make it beautiful

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-var express = require('express');
+(function () {
+  "use strict";
+  var express = require('express');
 //var routes = require('./routes');
-var http = require('http');
-var path = require('path');
+  var http = require('http');
+  var path = require('path');
 
-var app = express();
+  var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
 
-app.get('/', function (req, res) {
-  res.redirect('/index.html');
-});
+  app.get('/', function (req, res) {
+    res.redirect('/index.html');
+  });
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+})();
