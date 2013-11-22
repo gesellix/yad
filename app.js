31afa19019b9d92e5a6bfcac96ac8e2f59b398dd
Tobias Gesellchen <tobias@gesellix.de>
2013-11-22 18:04:05 +0100

    render view with handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function () {
   "use strict";
   var express = require('express');
+  var hbs = require('express-hbs');
   var dataprovider = require('./dataprovider');
   var http = require('http');
   var path = require('path');
@@ -8,10 +9,16 @@
   var app = express();
 
 // all environments
-//  app.engine('html', require('hogan-express'));
-//  app.enable('view cache');
-//  app.set('views', __dirname + '/views');
-//  app.set('view engine', 'html');
+  app.engine('hbs', hbs.express3({
+                                   defaultLayout: __dirname + '/views/layout/default.hbs',
+                                   layoutsDir: __dirname + '/views/layout',
+                                   partialsDir: __dirname + '/views/partials',
+                                   extname: '.hbs'
+//                                    contentHelperName: 'content'
+                                 }));
+  app.enable('view cache');
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hbs');
   app.set('port', process.env.PORT || 3000);
   app.use(express.favicon());
   app.use(express.logger('dev'));
@@ -22,16 +29,20 @@
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+  // production only
+  if ('production' == app.get('env')) {
+//    app.set('PROD_MODE', 'true');
+  }
+  // development only
   if ('development' == app.get('env')) {
     app.use(express.errorHandler());
   }
 
   dataprovider(app);
   app.get('/', function (req, res) {
-    res.redirect('/index.html');
-//    res.locals = {test: "test!"};
-//    res.render('index', {});
+//    res.redirect('/index.html');
+    res.locals = {title: "Yet Another Dashboard"};
+    res.render('index', {});
   });
 
   http.createServer(app).listen(app.get('port'), function () {
